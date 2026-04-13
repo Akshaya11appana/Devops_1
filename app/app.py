774c1f13c8247da749a970d37b6dfe47ea5d89ca
Akshaya11appana <118415379+Akshaya11appana@users.noreply.github.com>
2026-04-13 16:09:45 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,10 @@
+from flask import Flask
+import os
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return f"Hello from {os.getenv('ENV_NAME', 'UNKNOWN')} environment!"
+
+app.run(host="0.0.0.0", port=5000)
